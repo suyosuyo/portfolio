7e1409913b24f120d0b9d8fd1832d6af9ef32d1e
suyosuyo <149132459+suyosuyo@users.noreply.github.com>
2023-12-21 09:03:40 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,60 +1,4 @@
-// 横スクロール
-
-//    window.addEventListener("load", function(){
-
-// //プラグインを定義
-// gsap.registerPlugin(ScrollTrigger);
-
-// const area  = document.querySelector(".scroll");
-// const wrap  = document.querySelector(".mainscr");
-// const items = document.querySelectorAll(".secscr");
-// const num   = items.length;
-
-// gsap.set(wrap,  { width: num * 100 + "%" });//横幅を指定
-// gsap.set(items, { width: 100 / num + "%" });//横幅を指定
-
-// gsap.to(items, {
-//   xPercent: -100 * ( num - 1 ), //x方向に移動させる
-//   ease: "none",
-//   scrollTrigger: {
-//     trigger: area, //トリガー
-//     start: "top top", //開始位置
-//     end: "+=3000", //終了位置 スクロール量の幅調整
-//     pin: true, //ピン留め
-//     scrub: true, //スクロール量に応じて動かす
-//   }
-// });
-// });
-
-
-
-// フェードイン 画像
-//ウィンドウをスクロールさせたときに
-// $(window).scroll(function() {
-//   $('.fadein1,.fadein2,.fadein3').each(function() {
-//   //要素「hide」のTOPの縦位置を「hTop」とする
-//     var hTop = $(this).offset().top;
-//   //要素「hide」の高さを「hHeight」とする
-//     var hHeight = $(this).height();
-//   //要素「hide」のbottomの縦位置（TOPの縦位置+高さ）を「hBottom」とする
-//     var hBottom = hTop + hHeight;
-//   //ウィンドウ表示領域のTOPの縦位置を「wTop」とする
-//     var wTop = $(window).scrollTop();
-//   //ウィンドウ表示領域の高さを「wHeight」とする
-//     var wHeight = $(window).height();
-//   //ウィンドウ表示領域のbottomの縦位置（TOPの縦位置+高さ）を「wBottom」とする
-//     var wBottom = wTop + wHeight;
-//   //要素「hide」が完全にウィンドウ表示領域の内側に位置したら
-//     if (hBottom <= wBottom) {
-//   //要素「hide」の透明度を下げる
-//       $('.fadein1').css('display', 'block');
-//       // $('.fadein1').css('transition', 'opacity 1s;');
-//       $('.fadein2,.fadein3').css('opacity', '1');
-//       }
-//     })
-//   });
   
-
 
 // ナビゲーション 項目ごとに変化
 let targets = document.querySelectorAll('.block'); //アニメーションさせたい要素
@@ -70,6 +14,7 @@ window.addEventListener('scroll', function () {
   }
 }); 
 
+// ナビゲーション量でのアイコン変化はサイト構成のブクマに入ってる
 
 
 // レスポンシブ ナビゲーション
